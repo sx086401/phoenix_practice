ad1ab00917c5ef0dd86d910d6d63e9aa5410d835
Evan Tseng <evan.tseng@access-company.com>
2020-08-23 18:52:48 +0800

    [Fix] index

--- lib/phoenix_practice_web/controllers/book_controller.ex
+++ lib/phoenix_practice_web/controllers/book_controller.ex
@@ -4,7 +4,20 @@ defmodule PhoenixPracticeWeb.BookController do
   alias PhoenixPractice.Work
 
   def index(conn, _params) do
-    books = Work.get_books([])
+    books =
+      Work.get_books([], [preload: [:authors]])
+      |> Enum.map(fn work ->
+        %{
+          id: work.id,
+          title: work.title,
+          description: work.description,
+          publish_begin_at: work.publish_begin_at,
+          publish_end_at: work.publish_end_at,
+          search_text: work.search_text,
+          volume: work.volume,
+          authors: Enum.map(work.authors, & &1.name) |> Enum.join(","),
+        }
+      end)
     render(conn, "index.html", books: books)
   end
 
@@ -15,7 +28,7 @@ defmodule PhoenixPracticeWeb.BookController do
   # Example request body: %{category: :comic, book_authors: [%{author_id: 1}, ...]}
   def create(conn, book_params) do
     case Work.create_books(book_params) do
-      {:ok, res} ->
+      {:ok, _} ->
         send_resp(conn, 201, "Succeeded")
       {:error, _} ->
         json(conn, %{status: 500, description: "Some error had happened."})
